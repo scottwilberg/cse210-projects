b49fcfccbdcc9f0a8be1305eb7fc585e122645cd
scottwilberg <wil24112@byui.edu>
2025-01-08 06:49:12 -0700

    did exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter the score: ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your Grade is a {letterGrade}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. ");
+        }
+
     }
 }
