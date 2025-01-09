e104325af4e2ac9dafdf0a76eda5c2a92e83e812
scottwilberg <wil24112@byui.edu>
2025-01-08 18:32:50 -0700

    did exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // For Parts 1 and 2, where the user specified the number...
+        // Console.Write("What is the number? ");
+        // int number = int.Parse(Console.ReadLine());
+
+         // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("Enter a guess: ");
+            string letterGuess = Console.ReadLine();
+            guess = int.Parse(letterGuess);
+
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
