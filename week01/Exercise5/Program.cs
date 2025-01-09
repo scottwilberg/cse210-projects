52a2c23964dde1ac86fe039612a928a193a84c88
scottwilberg <wil24112@byui.edu>
2025-01-09 13:42:33 -0700

    did exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call the functions
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
+
+        //Create a function to display welcome message
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        //Create a function to get name and return it
+        static string PromptUserName()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        //Create a function to get users favorite number and return it
+        static int PromptUserNumber()
+        {
+            Console.Write("Enter your favorite number: ");
+            string number = Console.ReadLine();
+            int userNumber = int.Parse(number);
+            return userNumber;
+        }
+        //Create a function to get the square of a number and return it
+        static int SquareNumber(int number)
+        {
+            int squaredNumber = number * number;
+            return squaredNumber;
+        }
+        //Create a function that is passed in the name and squred number and displays them
+        static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        }
+
     }
 }
