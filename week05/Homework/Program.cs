983039018acc2b969af47cfafb895033e73d6e98
scottwilberg <wil24112@byui.edu>
2025-01-31 08:40:01 -0700

    week5 homework assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John", "Math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
